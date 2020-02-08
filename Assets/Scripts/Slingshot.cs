e67140b89e2622490c340283e96596f050cb00c9
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-08 17:36:02 -0500

    Launchpoint Halo on mouse

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+}
