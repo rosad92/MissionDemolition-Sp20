fe5a210debbcab8a8f5e6aedf2368c657c512a40
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-13 18:49:49 -0500

    fixed mouse drag

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -43,11 +43,14 @@ public class Slingshot : MonoBehaviour {
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
         Vector3 mouseDelta = mousePos3D - launchPos;
 
+
         float maxMagnitude = this.GetComponent<SphereCollider>().radius;
         if (mouseDelta.magnitude > maxMagnitude) {
             mouseDelta.Normalize();
             mouseDelta *= maxMagnitude; 
         }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
         if (Input.GetMouseButtonUp(0)) {
             aimingMode = false;
             projectileRigidBody.isKinematic = false;
